1a0a1c2051dfaffd461e7fd4def6a9a5154fb7ac
Min Hu <lebonthe@gmail.com>
2023-12-15 21:17:45 +0800

    final

---
+++ worldClock/Controller/RegionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RegionTableViewCell.swift
+//  worldClock
+//
+//  Created by Min Hu on 2023/12/15.
+//
+
+import UIKit
+
+class RegionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ worldClock/Model/TimeZone.swift
@@ -0,0 +1,8 @@
+//
+//  TimeZone.swift
+//  worldClock
+//
+//  Created by Min Hu on 2023/12/15.
+//
+
+import Foundation
